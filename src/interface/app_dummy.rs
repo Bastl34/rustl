4098436fbd7ea8490f32088280b1b500ac4f438a
Bastian Karge <bastian.karge@gmail.com>
2025-09-23 22:38:02 +0200

    testing

--- src/interface/app_dummy.rs
+++ src/interface/app_dummy.rs
@@ -376,7 +376,8 @@ impl App for AppDummy
             //let avatar_root = avatar_nodes.as_ref().unwrap()[0].clone();
 
             // //let _ = scene_utils::load_and_retarget_animation("objects/temp/Animation Only - Happy Idle.glb", scene_id, avatar_nodes.unwrap()[0], main_queue_clone.clone(),);
-            let _ = scene_utils::load_and_re_target_animation("resourcesLocal/objects/temp/dancing.glb", scene_id, avatar_nodes.unwrap()[0], main_queue_clone.clone(), Some("mixamorig:Hips"));
+            //let _ = scene_utils::load_and_re_target_animation("resourcesLocal/objects/temp/dancing.glb", scene_id, avatar_nodes.unwrap()[0], main_queue_clone.clone(), Some("mixamorig:Hips"));
+            let _ = scene_utils::load_and_re_target_animation("resourcesLocal/objects/temp/animations/shoot.glb", scene_id, avatar_nodes.unwrap()[0], main_queue_clone.clone(), None);
 
 
             //scene_utils::load_object("objects/temp/traffic_cone_game_ready.glb", scene_id, None, main_queue_clone.clone(), false, true, false, 0);
